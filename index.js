9e209c6edbda48edc371396993988b4433064276
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-01-28 19:13:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ var cantHear = ("I can't hear you!");
 var canHear = ("YES INDEED!");
 var loveYou = ("I love you, too.");
 
-if (string.toLowerCase.string) {
+if (string.toLowerCase.("hello!")) {
   return cantHear;
 }
